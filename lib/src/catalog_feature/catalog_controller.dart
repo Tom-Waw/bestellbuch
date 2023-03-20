1b305ea3d9053462e0d88168abf9cb8baaf80a79
Tom Wawerek <wawerektom@gmail.com>
2023-03-20 19:26:30 +0100

    fix: getx controller rebuild view

--- lib/src/catalog_feature/catalog_controller.dart
+++ lib/src/catalog_feature/catalog_controller.dart
@@ -5,48 +5,56 @@ import 'catalog.dart';
 
 /// A class to read and update the product catalog.
 class CatalogController extends GetxController {
+  RxBool isLoading = false.obs;
+
   late final CatalogService _catalogService;
-  late Catalog _root;
-  late Catalog _current;
+  late Rx<Catalog> _root;
+  late Rx<Catalog> _current;
 
   CatalogController(this._catalogService);
 
+  static CatalogController get to => Get.find<CatalogController>();
+
   @override
-  void onInit() async {
+  Future<void> onInit() async {
     super.onInit();
-    await loadCatalog();
+    loadCatalog();
   }
 
   // Allow Widgets to read the currently selected catalog.
-  Rx<Catalog> get root {
-    Catalog c = _current;
-    while (c.parent != _root) {
+  Catalog get current => _current.value;
+  // Allow Widgets to check wether currently on root catalog or not.
+  bool get isRoot => _current.value.parent == _root.value;
+  // Allow Widgets to read the root catalogs.
+  List<Catalog> get roots => _root.value.items.cast<Catalog>();
+  // Allow Widgets to read the currently selected catalog.
+  Catalog get selectedRoot {
+    Catalog c = _current.value;
+    while (c.parent != _root.value) {
       c = c.parent;
     }
-    return c.obs;
+    return c;
   }
-
-  // Allow Widgets to read the currently selected catalog.
-  Rx<Catalog> get current => _current.obs;
-  // Allow Widgets to check wether currently on root catalog or not.
-  Rx<bool> get isRoot => (_current.parent == _root).obs;
 
   /// Load the catalog from the CatalogService.
   Future<void> loadCatalog() async {
-    _root = await _catalogService.catalog();
-    _current = _root.items.first as Catalog;
+    isLoading(true);
+    Catalog catalog = await _catalogService.fetchData();
+    _root = catalog.obs;
+    _current = roots.first.obs;
+    isLoading(false);
   }
 
   /// Select catalog as current catalog.
   void select(CatalogItem catalog) {
     if (catalog is! Catalog) return;
-    _current = catalog;
+    _current(catalog);
   }
 
   /// Select catalog as current catalog.
   void back() {
-    if (_current.parent == _root) return;
-    _current = _current.parent;
+    if (_current.value.parent == _root.value) return;
+    _current(_current.value.parent);
   }
 
   /// Add and persist a new product based on the user's inputs.
@@ -54,18 +62,20 @@ class CatalogController extends GetxController {
     if (item == null) return;
 
     // Store the new Item in memory
-    _current.items.add(item);
-    item.setParent(_current);
+    _current.value.items.add(item);
+    item.setParent(_current.value);
     // Persist the changes to a database
     await _catalogService.add(item);
   }
 
   /// Delete an existing product or catalog completely.
   Future<void> delete(CatalogItem? item) async {
-    if (item == null || item == _root || item.parent == _root) return;
+    if (item == null || [item, item.parent].contains(_root.value)) {
+      return;
+    }
 
     // Remove Item from memory
-    _current.items.remove(item);
+    _current.value.items.remove(item);
     // Persist the changes to a database
     await _catalogService.delete(item);
   }
--- lib/src/catalog_feature/catalog_service.dart
+++ lib/src/catalog_feature/catalog_service.dart
@@ -3,22 +3,25 @@ import 'package:flutter/material.dart';
 import 'catalog.dart';
 
 class CatalogService {
-  Future<Catalog> catalog() async => Catalog(1, "main", Icons.check, [
-        Catalog(2, "Getränke", Icons.local_drink, [
-          Product(1, "Random Drink", 8.99),
-          Catalog(4, "Alkoholische Getränke", Icons.medication, [
-            Product(2, "Gin Tonic", 10.99),
-            Product(3, "Cuba Libre", 11.99),
+  Future<Catalog> fetchData() async => Future.delayed(
+        const Duration(seconds: 1),
+        () => Catalog(1, "main", Icons.check, [
+          Catalog(2, "Getränke", Icons.local_drink, [
+            Product(1, "Random Drink", 8.99),
+            Catalog(4, "Alkoholische Getränke", Icons.medication, [
+              Product(2, "Gin Tonic", 10.99),
+              Product(3, "Cuba Libre", 11.99),
+            ]),
+            Catalog(5, "Alkoholfreie Getränke", Icons.medication, [
+              Product(4, "Wasser", 4.99),
+            ]),
           ]),
-          Catalog(5, "Alkoholfreie Getränke", Icons.medication, [
-            Product(4, "Wasser", 4.99),
+          Catalog(3, "Speisen", Icons.food_bank, []),
+          Catalog(4, "Shishas", Icons.smoking_rooms, [
+            Product(5, "Artic Line", 7.99),
           ]),
         ]),
-        Catalog(3, "Speisen", Icons.food_bank, []),
-        Catalog(4, "Shishas", Icons.smoking_rooms, [
-          Product(5, "Artic Line", 7.99),
-        ]),
-      ]);
+      );
 
   Future<void> add(CatalogItem item) async {}
   Future<void> delete(CatalogItem item) async {}
