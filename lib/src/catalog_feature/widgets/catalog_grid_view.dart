07c47744fd6e189666f004a204d7e8f2e9d8cc90
Tom Wawerek <wawerektom@gmail.com>
2023-03-20 22:04:29 +0100

    add: sort and delete items

--- lib/src/catalog_feature/widgets/catalog_grid_view.dart
+++ lib/src/catalog_feature/widgets/catalog_grid_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../catalog.dart';
 import '../catalog_controller.dart';
 import 'catalog_item_box.dart';
 
@@ -10,13 +11,12 @@ class CatalogGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(() => GridView.count(
-          padding: const EdgeInsets.all(8.0),
-          crossAxisCount: 3,
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
-          children: CatalogController.to.current.items
-              .map((item) => CatalogItemBox(item: item))
-              .toList(),
-        ));
+        padding: const EdgeInsets.all(8.0),
+        crossAxisCount: 3,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        children: CatalogController.to.current.items
+            .map((item) => CatalogItemBox(item: item))
+            .toList()));
   }
 }
