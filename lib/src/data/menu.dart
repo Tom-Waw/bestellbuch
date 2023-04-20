c0528256d9cd6bd1fabb012b6613712944dbcebc
Jann Wawerek <jannwawerek@gmail.com>
2023-04-20 22:31:00 +0200

    WIP: Firebase data management

--- lib/src/data/menu.dart
+++ lib/src/data/menu.dart
@@ -1,15 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 abstract class MenuItem {
-  static int count = 0;
-
-  late final int id;
   Menu? _parent;
   final String name;
 
-  MenuItem(this.name) {
-    id = count++;
-  }
+  MenuItem(this.name);
 
   Menu get parent => _parent!;
   void setParent(Menu parent) {
@@ -32,10 +28,49 @@ class Menu extends MenuItem {
     if (_parent?._parent == null) return this;
     return _parent?.getRoot();
   }
+
+  factory Menu.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    var items = data?['items'] is Iterable ? List.from(data?['items']) : null,
+    items = items.map((item) => item.containsKey("price")? Product.fromFirestore(snapshot, options))
+    return Menu(
+      name: data?['name'],
+      items:
+          data?['items'] is Iterable ? List.from(data?['items']) : null,
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (name != null) "name": name,
+      if (items != null) "items": items,
+    };
+  }
 }
 
 class Product extends MenuItem {
   final double price;
 
   Product(super.name, this.price);
+
+  factory Product.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data();
+    return Product(
+      name: data?['name'],
+      price: data?['price'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (name != null) "name": name,
+      if (price != null) "price": price,
+    };
+  }
 }
